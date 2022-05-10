825448c3123f9a19a0c604da2cd6385b973f25a8
Silvio Junior <silvio.fs.junior@gmail.com>
2022-05-10 20:49:32 -0300

    Character form register and axios post request

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ function getActors() {
     .catch((error) => console.error(error));
 }
 
-getActors();
+// getActors();
 
 function getAge(birthyear, birthmonth, birthday) {
   var currDate = new Date(),
@@ -29,4 +29,42 @@ function getAge(birthyear, birthmonth, birthday) {
   }
 
   return age < 0 ? 0 : age;
+}
+
+function saveCharacter() {
+  var fdata = new FormData();
+
+  let nome = document.getElementById("iptName").value;
+  let tipo = document.getElementById("iptCharacterTypes");
+  let descricao = document.getElementById("iptDescription").value;
+  let imagem = document.getElementById("iptCustomFileChar").files[0];
+
+  let character = {
+    name: nome,
+    type: tipo.options[tipo.selectedIndex].text,
+    description: descricao
+  };
+
+  let characterBody = JSON.stringify(character);
+
+  fdata.append("character", characterBody);
+  fdata.append("image", imagem);
+
+  axios
+    .post(`http://localhost:8080/character`, fdata)
+    .then(function (response) {
+      cleanCharacterForm();
+      let closeModal = document.getElementById("closeModal");
+      closeModal.click();
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+function cleanCharacterForm() {
+  document.getElementById("iptName").value = "";
+  document.getElementById("iptCharacterTypes").value = 0;
+  document.getElementById("iptDescription").value = "";
+  document.getElementById("iptCustomFileChar").files[0] = "";
 }
