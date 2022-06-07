31972a0e243f6008bf4d5ae43eba9fd6bb3fe4eb
Silvio Junior <silvio.fs.junior@gmail.com>
2022-06-07 11:30:13 -0300

    list movies on index

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,11 @@ function getMovies() {
     .catch((error) => console.error(error));
 }
 
-getMovies();
-
 function renderMovieCard(movie) {
   //primeira DIV
   let card = document.createElement("div");
-  card.classList.add("card");
-  card.setAttribute("style", "width: 18rem");
+  card.classList.add("card", "mb-4","mx-2");
+  card.setAttribute("style", "width: 18rem; padding: 0;");
 
   //Movie Image
   let cardImg = document.createElement("img");
@@ -57,7 +55,7 @@ function renderMovieCard(movie) {
   releaseDateLi.classList.add("list-group-item");
 
   let releaseDateH6 = document.createElement("h6");
-  releaseDateH6.textContent = "Release Date:";
+  releaseDateH6.textContent = "Lançamento:";
 
   let releaseDateContent = document.createElement("span");
   releaseDateContent.setAttribute("id", "releaseDate");
@@ -70,7 +68,7 @@ function renderMovieCard(movie) {
   directionLi.classList.add("list-group-item");
 
   let directionH6 = document.createElement("h6");
-  directionH6.textContent = "Direction:";
+  directionH6.textContent = "Direção:";
 
   let directionContent = document.createElement("span");
   directionContent.setAttribute("id", "direction");
@@ -83,11 +81,16 @@ function renderMovieCard(movie) {
   budgetLi.classList.add("list-group-item");
 
   let budgetH6 = document.createElement("h6");
-  budgetH6.textContent = "Budget:";
+  budgetH6.textContent = "Orçamento:";
 
   let budgetContent = document.createElement("span");
   budgetContent.setAttribute("id", "budget");
-  budgetContent.textContent = movie.budget.toLocaleString('fullwide', {maximumFractionDigits:2, style:'currency', currency:'USD', useGrouping:true}) ;
+  budgetContent.textContent = movie.budget.toLocaleString("fullwide", {
+    maximumFractionDigits: 2,
+    style: "currency",
+    currency: "USD",
+    useGrouping: true
+  });
 
   budgetLi.append(budgetH6, budgetContent);
 
@@ -99,80 +102,12 @@ function renderMovieCard(movie) {
 
   let btnCharacters = document.createElement("a");
   btnCharacters.setAttribute("href", "#");
-  btnCharacters.classList.add("btn", "btn-primary","d-grid", "gap-2");
-  btnCharacters.textContent = "Characters"
+  btnCharacters.classList.add("btn", "btn-primary", "d-grid", "gap-2");
+  btnCharacters.textContent = "Personagens";
 
   characters.appendChild(btnCharacters);
 
   card.append(cardImg, titleAndSubtitle, movieAttributes, characters);
 
   document.getElementById("listMovies").appendChild(card);
-}
-
-function getAge(birthyear, birthmonth, birthday) {
-  var currDate = new Date(),
-    currYear = currDate.getFullYear(),
-    currMonth = currDate.getMonth() + 1,
-    currDay = currDate.getDate(),
-    birthyear = +birthyear,
-    birthmonth = +birthmonth,
-    birthday = +birthday,
-    age = currYear - birthyear;
-
-  if (
-    currMonth < birthmonth ||
-    (currMonth == birthmonth && currDay < birthday)
-  ) {
-    age--;
-  }
-
-  return age < 0 ? 0 : age;
-}
-
-function saveCharacter() {
-  var fdata = new FormData();
-
-  /*   let nome = document.getElementById("iptName").value;
-  let tipo = document.getElementById("iptCharacterTypes");
-  let descricao = document.getElementById("iptDescription").value;
-  let imagem = document.getElementById("iptCustomFileChar").files[0]; */
-
-  let nome = iptName.value;
-  let tipo = iptCharacterTypes;
-  let descricao = iptDescription;
-  let imagem = iptCustomFileChar.files[0];
-
-  let character = {
-    name: nome,
-    type: tipo.options[tipo.selectedIndex].text,
-    description: descricao
-  };
-
-  let characterBody = JSON.stringify(character);
-
-  fdata.append("character", characterBody);
-  fdata.append("image", imagem);
-
-  axios
-    .post(`http://localhost:8080/character`, fdata)
-    .then(function (response) {
-      cleanCharacterForm();
-      let closeModal = document.getElementById("closeModal");
-      closeModal.click();
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-}
-
-function cleanCharacterForm() {
-  /*   document.getElementById("iptName").value = "";
-  document.getElementById("iptCharacterTypes").value = 0;
-  document.getElementById("iptDescription").value = "";
-  document.getElementById("iptCustomFileChar").files[0] = ""; */
-
-  iptName.value = "";
-  iptCharacterTypes.value = 0;
-  iptDescription.value = "";
-  iptCustomFileChar.files[0] = "";
 }
